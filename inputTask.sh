b68c0fafe213ff2614a2d3f0b07bbd4b730eb72a
salim0896 <56827435+salim0896@users.noreply.github.com>
2020-04-06 21:02:24 +0100

    Add files via upload

---
+++ inputTask.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+echo "What is your favourite food?"
+read food
+
+echo "What is the capital of your native country?"
+read capital
+
+echo "What is your favourite season?"
+read season
+
+echo ""
+
+echo "Nice, Your favourite food is $food, the capital of your native country 
+is $capital and your favourite season of the year is $season."
+
+
+
+
+
+
+
+#args=("@")
+
+#echo $@
+#echo $#
